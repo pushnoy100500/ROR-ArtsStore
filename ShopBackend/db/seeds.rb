55eb9feaa6bfaf9107b7ebddf92c3ecf10253547
pushnoy100500 <brainstation@BrainStations-MacBook-Air-6.local>
2015-07-28 15:34:35 -0400

    dummy data from db

--- ShopBackend/db/seeds.rb
+++ ShopBackend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: 'Picture #1', image_url: 'dummy-icon.png', price: 2.50, user_id: 1, 
+				overview: 'quick overview of the current picture', 
+				specification: 'dummy specification that is supposed to take more space than it is required')
+Product.create(name: 'Picture #2', image_url: 'dummy-icon.png', price: 4.53, user_id: 1, 
+				overview: 'quick overview of the current picture', 
+				specification: 'dummy specification that is supposed to take more space than it is required')
+Product.create(name: 'Picture #3', image_url: 'dummy-icon.png', price: 1.20, user_id: 1, 
+				overview: 'quick overview of the current picture', 
+				specification: 'dummy specification that is supposed to take more space than it is required')
+Product.create(name: 'Picture #4', image_url: 'dummy-icon.png', price: 32.2, user_id: 1, 
+				overview: 'quick overview of the current picture', 
+				specification: 'dummy specification that is supposed to take more space than it is required')
+Product.create(name: 'Picture #5', image_url: 'dummy-icon.png', price: 50, user_id: 1, 
+				overview: 'quick overview of the current picture', 
+				specification: 'dummy specification that is supposed to take more space than it is required')
