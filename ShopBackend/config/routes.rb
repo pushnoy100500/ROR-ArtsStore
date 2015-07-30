ed5cef7c154e45fa2612e17dc38162f7697c55c8
pushnoy100500 <brainstation@BrainStations-MacBook-Air-6.local>
2015-07-30 15:01:28 -0400

    before merge

--- ShopBackend/config/routes.rb
+++ ShopBackend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root "products#welcome_index"
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
