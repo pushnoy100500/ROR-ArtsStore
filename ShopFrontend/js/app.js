50dd3da6de038725fc0dddc980bb518e115793ec
pushnoy100500 <brainstation@BrainStations-MacBook-Air-6.local>
2015-07-30 16:19:33 -0400

    ajax request products.son

--- ShopFrontend/js/app.js
+++ ShopFrontend/js/app.js
@@ -1,10 +1,27 @@
 var app = angular.module('App',['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $httpProvider) {
   $routeProvider
     .when('/home', {
         templateUrl: 'templates/main.html',
-        controller: 'SimpleCtrl as ctrl'
+        controller: 'ListingCtrl as ctrl',
+        resolve: {
+        	catalogue: function($http, $q) {
+	        	var req = {
+	            method: "GET",
+	            url: "http://localhost:3000/products.json",
+        		}
+        	var defer = $q.defer();
+        	$http(req).success(function(data) {
+        		console.log(data);
+        		console.log('success');
+        		defer.resolve(data);
+        	}).error(function(data) {
+        		console.log(data);
+        	});
+        	return defer.promise;
+        }
+    }
       })
       .otherwise({
         redirectTo: '/home'
---
+++ ShopFrontend/js/listingCtrl.js
@@ -0,0 +1,7 @@
+function ListingCtrl(catalogue) {
+	this.catalogue = catalogue;
+}
+ListingCtrl.prototype.logShit = function(){
+	console.log(this.catalogue);
+};
+app.controller('ListingCtrl', ListingCtrl);
